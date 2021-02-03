576b06a97f97070f634d3c28039f5cd93279d9ed
themashina <amosov00@gmail.com>
2021-02-03 16:34:25 +0200

    commit

---
+++ src/components/line.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="chart-demo">
+    <DxChart
+        id="chart"
+        :data-source="dataSource"
+        palette="Violet"
+    >
+      <DxCommonSeriesSettings
+          argument-field="country"
+      />
+      <DxSeries
+          v-for="energy in energySources"
+          :key="energy.value"
+          :value-field="energy.value"
+          :name="energy.name"
+      />
+      <DxMargin :bottom="20"/>
+      <DxArgumentAxis
+          :value-margins-enabled="false"
+          discrete-axis-division-mode="crossLabels"
+      >
+        <DxGrid :visible="true"/>
+      </DxArgumentAxis>
+      <DxLegend
+          vertical-alignment="bottom"
+          horizontal-alignment="center"
+          item-text-position="bottom"
+      />
+      <DxTooltip :enabled="true"/>
+    </DxChart>
+  </div>
+</template>
+<script>
+import {
+  DxChart,
+  DxSeries,
+  DxArgumentAxis,
+  DxCommonSeriesSettings,
+  DxExport,
+  DxGrid,
+  DxMargin,
+  DxLegend,
+  DxTitle,
+  DxSubtitle,
+  DxTooltip
+} from 'devextreme-vue/chart';
+import DxSelectBox from 'devextreme-vue/select-box';
+
+import service from './data.js';
+
+export default {
+
+  components: {
+    DxSelectBox,
+    DxChart,
+    DxSeries,
+    DxArgumentAxis,
+    DxCommonSeriesSettings,
+    DxExport,
+    DxGrid,
+    DxMargin,
+    DxLegend,
+    DxTitle,
+    DxSubtitle,
+    DxTooltip
+  },
+
+  data() {
+    return {
+      dataSource: service.getCountriesInfo(),
+      energySources: service.getEnergySources(),
+    };
+  }
+};
+</script>
+<style>
+.option {
+  margin-top: 10px;
+}
+
+.caption {
+  font-size: 18px;
+  font-weight: 500;
+}
+
+.option > span {
+  margin-right: 14px;
+}
+
+.option > .dx-widget {
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import line from "@/components/line";
 
 Vue.config.productionTip = false
+
+Vue.component('t-line', line)
 
 new Vue({
   render: h => h(App),
